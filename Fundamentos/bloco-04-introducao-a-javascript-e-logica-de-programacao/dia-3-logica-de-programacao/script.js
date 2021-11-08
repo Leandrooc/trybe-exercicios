75ec5fcdf9101c02c63c2dbe00eb3bf690113fcf
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-11-08 20:08:52 -0300

    Exercício 1 e 2

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao/script.js
@@ -0,0 +1,30 @@
+/* Exercício 1 */
+
+console.log("Exercício 1")
+
+n = 10
+asterisco = "*"
+linhas = ""
+
+for (index = 0; index < n; index++) {
+    linhas = linhas += asterisco
+
+}
+
+for (index = 0; index < n; index++) {
+        console.log(linhas)
+}
+
+/* Exercício 2 */
+
+console.log("Exercício 2")
+
+n2 = 6
+asterisco2 = "*"
+linhas2 = ""
+
+for (index = 0; index < n2; index++) {
+       linhas2 += asterisco2
+       console.log(linhas2)
+        
+}
