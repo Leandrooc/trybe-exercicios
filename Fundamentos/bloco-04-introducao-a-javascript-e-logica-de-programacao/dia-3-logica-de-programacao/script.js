4c118cd501e524b8e3214ab5bf49cfa5cced37ad
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-11-09 19:41:42 -0300

    Exercício da aula ao vivo adicionado, e exercício 6

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao/script.js
@@ -1,4 +1,4 @@
-/* Exercício 1 */
+/* Exercício 1 */ 
 
 console.log("Exercício 1")
 
@@ -15,7 +15,7 @@ for (index = 0; index < n; index++) {
         console.log(linhas)
 }
 
-/* Exercício 2 */
+/* Exercício 2 */ 
 
 console.log("Exercício 2")
 
@@ -25,6 +25,61 @@ linhas2 = ""
 
 for (index = 0; index < n2; index++) {
        linhas2 += asterisco2
+
        console.log(linhas2)
         
 }
+
+/* Exercício 6 */ 
+
+let numeroprimo = 32
+let divisores = [];
+
+for( index = 0; index <= numeroprimo; index ++) {
+
+    if (numeroprimo % index === 0 && numeroprimo > 0){
+        divisores.push(index)
+    }
+
+    if (index == numeroprimo && divisores.length == 2) {
+
+        console.log(" É um número primo, pois possui apenas 2 divisores, 1 e", divisores[1])
+        console.log(divisores)
+
+    }  
+    
+    if (index == numeroprimo && divisores.length > 2) {
+
+        console.log(" Não é um número primo, pois possui mais de apenas 2 divisores.")
+        console.log(divisores)
+
+    }
+
+} 
+
+
+/* Aula ao vivo exercício, funcionalidade adicionada */
+
+let qtd = 10;
+let ingredientes = ["presunto","maionese"];
+let sacola = [];
+let intolerante = false
+
+
+for(let index = 0; index < 10; index+=1){
+
+let pao = [];
+
+for( let index = 0; index < ingredientes.length; index+=1){
+pao.push(ingredientes[index])
+
+}
+sacola.push(pao);
+
+if(intolerante === false) {
+    pao.push("queijo")
+}
+
+} 
+
+console.log(sacola)
