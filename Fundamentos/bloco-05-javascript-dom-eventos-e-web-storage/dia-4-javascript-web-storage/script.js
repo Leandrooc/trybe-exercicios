daff43dc6823e0f7962922bcb5bd84b220a63c76
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-11-20 18:45:46 -0300

    Exercícios 4 e 5 :rocket:

--- Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -34,3 +34,27 @@ input3.addEventListener("change", (event) => {
 })
 
 p.style.fontSize = localStorage.getItem('fontp')
+
+// Exercício 4
+
+let input4 = document.getElementById("input4");
+
+input4.addEventListener("change", (event) => {
+    let evtpx = event.target.value + "px"
+    p.style.lineHeight = evtpx
+    localStorage.setItem('lineh', evtpx)
+})
+
+p.style.lineHeight = localStorage.getItem('lineh')
+
+// Exercício 5
+
+let input5 = document.getElementById("input5");
+
+input5.addEventListener("change", (event) => {
+    let evtpx = event.target.value
+    p.style.fontFamily = evtpx
+    localStorage.setItem('fontf', evtpx)
+})
+
+p.style.fontFamily = localStorage.getItem('fontf')
