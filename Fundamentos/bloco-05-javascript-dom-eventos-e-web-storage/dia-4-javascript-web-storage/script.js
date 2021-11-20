03b4057ec0845e3c3e2a966321c8ee5d64269bae
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-11-20 16:57:18 -0300

    Exercícios 2 e 3 :rocket:

--- Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,6 +1,8 @@
+// Exercício 1
+
 let input = document.getElementById("input");
 
-let bcolor = input.addEventListener("change", (event) => {
+let bcolor = input.addEventListener("change", () => {
     let coloruser = input.value
     document.body.style.backgroundColor = coloruser
     localStorage.setItem('bgcolor', coloruser)
@@ -8,3 +10,27 @@ let bcolor = input.addEventListener("change", (event) => {
 
 document.body.style.backgroundColor = localStorage.getItem('bgcolor')
 
+// Exercício 2
+
+let input2 = document.getElementById("input2");
+let p = document.getElementsByTagName("p")[0];
+
+input2.addEventListener("change", () => {
+    p.style.color = input2.value
+    localStorage.setItem('txtcolor', input2.value)
+})
+
+p.style.color = localStorage.getItem('txtcolor')
+
+
+// Exercício 3
+
+let input3 = document.getElementById("input3");
+
+input3.addEventListener("change", (event) => {
+    let evtpx = event.target.value + "px"
+    p.style.fontSize = evtpx
+    localStorage.setItem('fontp', evtpx)
+})
+
+p.style.fontSize = localStorage.getItem('fontp')
