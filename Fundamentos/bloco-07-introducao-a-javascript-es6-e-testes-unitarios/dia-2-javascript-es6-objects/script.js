1e97dbb479ba2d0726b635f4cb305723f14d4404
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-12-06 17:16:28 -0300

    :rocket:

---
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -0,0 +1,121 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+   console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R.
+   ${order.address.street}, Número: ${order.address.number}, AP: ${order.address.apartment}`)
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+order.name = 'Luiz Silva';
+order.order.pizza.marguerita.price = 20;
+console.log(`Olá ${order.name}, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é 
+${order.order.pizza.marguerita.price + order.order.drinks.coke.price + order.order.pizza.pepperoni.price}`)
+};
+
+orderModifier(order);
+
+// Parte 2
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addturn = (obj,key,value) =>  obj[key] = value;
+ 
+addturn(lesson2,'turno', 'noite')
+console.log(lesson2)
+
+const listkeys = (obj) => {console.log(Object.keys(obj))}
+listkeys(lesson1)
+
+const listkeyslength = (obj) => {console.log(Object.keys(obj).length)}
+listkeyslength(lesson1)
+
+const objvalue = (obj) => {console.log(Object.values(obj))}
+objvalue(lesson1)
+
+const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
+console.log(allLessons)
+
+let estudantes = 0;
+
+const aulas = () => {
+  let entries = Object.entries(allLessons)
+  for(property in entries) {
+    estudantes = estudantes + entries[property][1].numeroEstudantes
+  }
+  console.log(estudantes)
+}
+aulas();
+
+//
+
+const getValueByNumber = (obj, position) => console.log(Object.values(obj)[position]);
+
+getValueByNumber(lesson1, 0)
+
+
+//
+
+const verifyPair = (obj,name,keyvalue) => {
+  if(obj[name] === keyvalue) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+verifyPair(lesson3, 'turno', 'noite');
+verifyPair(lesson3, 'materia', 'Maria Clara');
