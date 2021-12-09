fe9161d3c049156d2a814697b20126e45c09a324
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-12-09 16:27:18 -0300

    Exercícios 1 e 2 :rocket:

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -6,3 +6,39 @@ const sleep = () => 'Partiu dormir!!';
 const doingThings = (func) => console.log(func());
 
 doingThings(sleep);
+
+// Exercício 1;
+
+const c = (nomeCompleto) =>  {
+  let emails = nomeCompleto.replace(/ /g, "_");
+  emails = emails + '@trybe.com'
+  return {nomeCompleto, email: emails,}
+}
+
+const newEmployees = (newobj,newobj2,newobj3) => {
+  const employees = {
+    id1: newobj, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: newobj2, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: newobj3, // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(c('Pedro Guerra'),c('Luiza Drumond'), c('Carla Paiva')));
+
+// Exercício 2
+
+const lucky = (num,luckyest) => {
+  if(num === luckyest) 
+    return 'Parabéns você ganhou'
+    else return 'Tente novamente'
+};
+
+const luckygenerator = (num, func) => {
+  const luckyest = (Math.floor(Math.random() * (5)) + 1)
+  return func(num,luckyest)
+}
+
+console.log(luckygenerator(3,lucky));
+
+// Exercício 3
