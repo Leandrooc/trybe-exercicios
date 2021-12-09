bd067191b5a4089b18c413ecd9dbd0460c5fec9c
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2021-12-09 15:29:55 -0300

    Exercício de fixação

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,8 @@
+// Partiu fixar o conteúdo;
+
+const wakeUp = () => 'Acordando!!';
+const coffe = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+const doingThings = (func) => console.log(func());
+
+doingThings(sleep);
